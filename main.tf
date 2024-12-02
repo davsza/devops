0a339ebcf8a17e05bf599ea90fb37fab3f74cfbf
davsza <david.szabolcs115@gmail.com>
2024-12-02 21:31:36 +0100

    tf

--- main.tf
+++ main.tf
@@ -12,27 +12,15 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
-# Közös hálózat létrehozása
 resource "docker_network" "monitoring_network" {
   name = "${var.project_name}-network"
   driver = "bridge"
   # Enable IPv6 if needed
   ipam_config {
-    subnet = "172.18.0.0/16"  # Customize subnet as needed
+    subnet = "172.18.0.0/16"
     gateway = "172.18.0.1"
   }
   internal = false
-}
-
-# NodeJS alkalmazás modul
-module "nodejs_app" {
-  source = "./modules/nodejs-app/docker"
-  
-  app_port = var.app_port
-  container_name = "${var.project_name}-nodejs"
-  network = docker_network.monitoring_network.name
-
-  depends_on = [docker_network.monitoring_network]
 }
 
 # Prometheus modul
@@ -49,24 +37,6 @@ module "grafana" {
   
   network = docker_network.monitoring_network.name
   prometheus_url = "http://prometheus:9090"
-}
-
-# Zabbix modul
-module "zabbix" {
-  source = "./modules/zabbix"
-  
-  network = docker_network.monitoring_network.name
-  mysql_root_password = var.mysql_root_password
-  zabbix_mysql_password = var.zabbix_mysql_password
-}
-
-# Graylog modul
-module "graylog" {
-  source = "./modules/graylog"
-  
-  network                    = docker_network.monitoring_network.name
-  graylog_password_secret    = var.graylog_password_secret
-  graylog_root_password_sha2 = var.graylog_root_password_sha2
 }
 
 output "network_info" {
---
+++ variables.tf
@@ -0,0 +1,18 @@
+# variables.tf
+variable "project_name" {
+  description = "A projekt neve, ami az erőforrások elnevezésében is megjelenik"
+  type        = string
+  default     = "devops"
+}
+
+variable "environment" {
+  description = "Környezet neve (dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "app_port" {
+  description = "Az alkalmazás portja"
+  type        = number
+  default     = 3000
+}
