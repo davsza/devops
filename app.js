fed47a5fa18f9d801bb3226841b49c3566d7d322
davsza <david.szabolcs115@gmail.com>
2024-12-02 19:52:59 +0100

    app.js change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hi, devops!');
+    res.send('Hi, devops! I\'m a change');
 });
 
 app.listen(3000, () => {
